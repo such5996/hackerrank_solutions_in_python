ffc150aeedd61312ec660f75010cd8b7e2a1374b
such5996 <87171715+such5996@users.noreply.github.com>
2021-09-18 20:39:33 +0530

    Create CamelCase.py

---
+++ CamelCase.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    s = list(s)
+    c = 1
+    for i in range(len(s)):
+        if(s[i].isupper()):
+            c+=1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
