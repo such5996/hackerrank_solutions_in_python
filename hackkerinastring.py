62a910bc9f51b6d0d5e68c7eddb32c00e545a310
such5996 <87171715+such5996@users.noreply.github.com>
2021-09-18 20:43:50 +0530

    Create hackkerinastring.py

---
+++ hackkerinastring.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerrankInString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def hackerrankInString(s):
+    # Write your code here
+    h ='hackerrank'
+    a = 0
+    for i in s:
+        if (a<10 and i == h[a]):
+            a +=1
+    if(a == 10):
+        return 'YES'
+    else:
+        return 'NO'
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = hackerrankInString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
